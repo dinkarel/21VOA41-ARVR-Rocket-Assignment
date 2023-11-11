3c019ae331a71484405ecdbc155ebf9ca95b035a
dinkarel <150506175+dinkarel@users.noreply.github.com>
2023-11-11 12:41:50 +0530

    Add files via upload

---
+++ rocket_source/skyrockets/explosions.js
@@ -0,0 +1,27 @@
+/*
+	This file contains definitions for skyrocket explosion shapes.
+	Colours are defined in colors.js which allows us to easily randomise the explosion and colour combinations.
+	You can create your own exlosion shapes using tools/designer.html including loading and altering those defined here.
+*/
+var explosions =
+[
+	{
+		"name": "Boom",
+		"creator": "A. Person",
+		"duration": 1,
+		"power": "Power1",
+		"ease": "easeOut",
+		"particles":
+		[
+		  {"x": 101, "y": -41, "r": 10, "c": 1, "a": 0.8},
+		  {"x": 285, "y": -53, "r": 10, "c": 1, "a": 0.8},
+		  {"x": -433, "y": -109, "r": 10, "c": 2, "a": 0.8},
+		  {"x": -289, "y": -70, "r": 10, "c": 2, "a": 0.8},
+		  {"x": -337, "y": 90, "r": 10, "c": 3, "a": 0.8},
+		  {"x": -259, "y": 74, "r": 10, "c": 3, "a": 0.8},
+		  {"x": -259, "y": -16, "r": 10, "c": 3, "a": 0.8},
+		  {"x": -93, "y": -61, "r": 10, "c": 3, "a": 0.8},
+		  {"x": -76, "y": 27, "r": 10, "c": 3, "a": 0.8}
+		]
+	  }
+];
